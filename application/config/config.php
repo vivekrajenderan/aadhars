49c2402817173454295c8a3f6818982d8e9fc72f
vivekrajenderan <vivekinnoppl@gmail.com>
2016-08-25 13:14:15 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/aadhar/';
+$config['base_url']	= 'http://localhost/aadhars/';
 
 /*
 |--------------------------------------------------------------------------
